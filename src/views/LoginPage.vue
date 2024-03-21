968bdf869084a0882bdf4e703a53327de0f010da
didgu <didgud0822@gmail.com>
2024-03-21 11:22:43 +0900

    비밀번호 찾기 절차 UI 변경작업 완료

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -76,7 +76,7 @@
             ></v-checkbox>
             <!-- ▼ 비밀번호 찾기 관련 Dialog(모달)  -->
             <v-dialog
-              v-model="dialog"
+              v-model="newPasswordDialog"
               persistent
               max-width="300px"
             >
@@ -91,11 +91,10 @@
                   비밀번호 찾기
                 </v-btn>
               </template>
-              <!-- ▼ 비밀번호 찾기 (임시 비밀번호 발급) 모달 창 -->
-              <v-card>
-                <v-card-title>
-                  <span class="text-h6">임시 비밀번호</span>
-                </v-card-title>
+              <!-- ▼ 비밀번호 찾기 (확인코드 발급) 모달 창 -->
+              <v-card v-if="!setPassword">
+                <p class="text-center text-h6 font-weight-black mt-5 mb-0 primary--text">비밀번호 재설정</p>
+                <p class="text-caption text-center mt-3">아이디를 기입해주세요.<br>사용자 메일로 확인코드를 발송합니다.</p>
                 <v-card-text>
                   <v-container>
                     <v-row>
@@ -103,19 +102,12 @@
                         cols="12"
                         class="pb-0">
                         <v-text-field
-                          label="성함"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        class="pt-0">
-                        <v-text-field
-                          label="Email"
+                          label="ID"
+                          v-model="userID"
                         ></v-text-field>
                       </v-col>
                     </v-row>
                   </v-container>
-                  <p class="text-caption text-center">이메일로 임시 비밀번호가 발급됩니다</p>
                 </v-card-text>
                 <v-card-actions>
                   <v-spacer></v-spacer>
@@ -135,6 +127,45 @@
                   </v-btn>
                 </v-card-actions>
               </v-card>
+              <v-card v-if="setPassword">
+                <p class="text-center text-h6 font-weight-black mt-5 mb-0 primary--text">비밀번호 재설정</p>
+                <p class="text-caption text-center mt-3">발급된 확인코드와 <br> 새 비밀번호를 입력해주세요</p>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        class="pb-0">
+                        <v-text-field
+                          label="확인코드"
+                          v-model="verificationCode"
+                        ></v-text-field>
+                        <v-text-field
+                          label="새 비밀번호"
+                          v-model="setNewPassword"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="dialog = false"
+                  >
+                    닫기
+                  </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="setNewPasswordFunc()"
+                  >
+                    변경
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
             </v-dialog>
           </v-col>
         </v-row>
@@ -151,7 +182,11 @@ export default {
   },
   data(){
     return{
-        dialog: false,
+        newPasswordDialog: false,
+        setPassword: false,
+        userID:'',
+        verificationCode:'',
+        setNewPassword:'',
         checkbox: Vue.$cookies.keys().includes(this.$configJson.cookies.id),
         idValue: Vue.$cookies.keys().includes(this.$configJson.cookies.id) ? Vue.$cookies.get(this.$configJson.cookies.id) : ''
     }
@@ -159,7 +194,16 @@ export default {
   methods: {
     // ▼ 비밀번호 재발급 함수, 아직은 모달만 열림
     reissuePasswordFunc(){
-      this.dialog = false;
+      alert("사용자 메일로 확인코드가 발송되었습니다!");
+      this.setPassword = true;
+    },
+    setNewPasswordFunc(){
+      alert("비밀번호 변경이 완료되었습니다!");
+      this.newPasswordDialog = false;
+      this.setPassword = false;
+      this.userID = '';
+      this.verificationCode = '';
+      this.setNewPassword = '';
     },
     async login(){
       const id = document.getElementById('idField').value;
@@ -185,7 +229,7 @@ export default {
             case 'id':
               alert('존재하지 않는 아이디');
               break;
-          
+
             default:
               alert(err.message);
               break;
