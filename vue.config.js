b9b4294bb79f174d73eae7221bae238293276f02
Min <[minitree45@naver.com](mailto:minitree45@naver.com)>
2024-03-27 19:29:18 +0900

    C:/Program Files/Git/api 하위 url 모두 대응 가능하도록 proxy 설정

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,22 @@ module.exports = defineConfig({
     'vuetify'
   ],
   devServer: {
-    proxy: 'http://192.168.0.5:8000'
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.5:8000',
+        changeOrigin: true,
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      },
+      // '/another-api': {
+      //   target: 'http://another-api.example.com',
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '^/another-api': ''
+      //   }
+      }
+      // 추가적인 URL 패턴에 대한 프록시 설정을 필요에 따라 추가할 수 있습니다.
   },
   configureWebpack: {
     devtool: 'source-map'
