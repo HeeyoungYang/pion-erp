c478f0e0bfed105614fbe3088daa520b0ac5dbaa
Min <[minitree45@naver.com](mailto:minitree45@naver.com)>
2024-05-03 14:00:38 +0900

    썸네일 관련 함수 Promise 함수로 변환

--- src/mux.js
+++ src/mux.js
@@ -735,24 +735,42 @@ mux.Util = {
   },
 
   uint8ArrayToHexString(uint8Array) {
-      return '0x' + Array.from(uint8Array, byte => ('0' + (byte & 0xFF).toString(16)).slice(-2)).join('');
+    return new Promise((resolve, reject) => {
+      try {
+        resolve('0x' + Array.from(uint8Array, byte => ('0' + (byte & 0xFF).toString(16)).slice(-2)).join(''));
+      } catch (error) {
+        reject(error);
+      }
+    });
   },
 
   hexToUint8Array(hexString) {
-    if (hexString.length % 2 !== 0) {
-        throw new Error('Invalid hex string');
-    }
-    const uint8Array = new Uint8Array(hexString.length / 2);
-    for (let i = 0; i < hexString.length; i += 2) {
-        uint8Array[i / 2] = parseInt(hexString.substr(i, 2), 16);
-    }
-    return uint8Array;
+    return new Promise((resolve, reject) => {
+      try {
+        if (hexString.length % 2 !== 0) {
+            throw new Error('Invalid hex string');
+        }
+        const uint8Array = new Uint8Array(hexString.length / 2);
+        for (let i = 0; i < hexString.length; i += 2) {
+            uint8Array[i / 2] = parseInt(hexString.substr(i, 2), 16);
+        }
+        resolve(uint8Array);
+      } catch (error) {
+        reject(error);
+      }
+    });
   },
 
 
 
   binaryToURL(binary){
-    return URL.createObjectURL(new Blob([binary]));
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(URL.createObjectURL(new Blob([binary])));
+      } catch (error) {
+        reject(error);
+      }
+    });
   },
 
   /**
